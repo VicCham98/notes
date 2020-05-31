ec84082372d1291d15661268c20c437b90f84f98
VicCham98 <41102257+VicCham98@users.noreply.github.com>
2020-05-31 17:14:26 -0500

    modifies

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          este es un proyecto sin modificar
         </p>
         <a
           className="App-link"
